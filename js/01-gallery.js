fb6247891cddb076ed0d395deca6dac5339aa928
CageyOwl <alex1377c9@gmail.com>
2023-01-03 01:23:54 +0200

    First commit

---
+++ js/01-gallery.js
@@ -0,0 +1,43 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const instanceLB = basicLightbox.create(
+  `<img id="modalImg" src="./" loading="lazy">`
+);
+
+function escListening(e) {
+  if (e.key === "Escape") {
+    instanceLB.close();
+    document.body.removeEventListener("keydown", escListening);
+  }
+}
+
+const gallery = document.querySelector(".gallery");
+const galleryContentHTML = galleryItems.reduce((str, img) => {
+  return (str += `
+        <div class="gallery__item">
+            <a class="gallery__link" href=${img.original}>
+                <img
+                    class="gallery__image"
+                    src=${img.preview}
+                    data-source=${img.original}
+                    alt="${img.description}"
+                    loading="lazy"
+                />
+            </a>
+        </div>
+        `);
+}, "");
+gallery.insertAdjacentHTML("afterbegin", galleryContentHTML);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  instanceLB.element().querySelector("#modalImg").src =
+    event.target.dataset.source;
+  instanceLB.show();
+
+  document.body.addEventListener("keydown", escListening);
+});
---
+++ js/02-lightbox.js
@@ -0,0 +1,28 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const gallery = document.querySelector(".gallery");
+const galleryContentHTML = galleryItems.reduce((str, img) => {
+  return (str += `
+        <a class="gallery__item" href=${img.original}>
+            <img
+                class="gallery__image"
+                src=${img.preview}
+                alt="${img.description}"
+                loading="lazy"
+            />
+        </a>
+        `);
+}, "");
+gallery.insertAdjacentHTML("afterbegin", galleryContentHTML);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  docClose: false,
+  animationSpeed: 250,
+  captionSelector: "img",
+  captionsData: "alt",
+});
